ff9c4cee94814551462370a8dca57a74025758bd
Aaryan345 <130822740+Aaryan345@users.noreply.github.com>
2023-06-09 21:21:52 +0530

    Update task3.js

---
+++ task3.js
@@ -0,0 +1,10 @@
+function addAndCapitalizeStrings(str1, str2) {
+  var result = str1 + str2;
+  return result.toUpperCase();
+}
+
+// Example usage
+var string1 = "Hello";
+var string2 = "World";
+var combinedStrings = addAndCapitalizeStrings(string1, string2);
+console.log(combinedStrings);
