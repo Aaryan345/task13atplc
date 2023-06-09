5b351072a3fa460df77d96f063f47a81d04b3c28
Aaryan345 <130822740+Aaryan345@users.noreply.github.com>
2023-06-09 21:20:30 +0530

    Update task.js

---
+++ task.js
@@ -0,0 +1,9 @@
+function printNameAndAddress() {
+  var name = "aaryan";
+  var address = "motihari";
+
+  console.log("Name:", name);
+  console.log("Address:", address);
+}
+
+printNameAndAddress();
